eee3cd88f3808d676c908784568717d5b751eacb
shrishti-m <vibhanshuverma.nature@gmail.com>
2024-09-04 11:29:30 +0000

    updated

---
+++ c_intro/helloworld.c
@@ -0,0 +1,10 @@
+/* This is a hello world program
+   Owner: Shrishti Date: 02-09-2024 */
+
+#include<stdio.h>
+int main()
+{
+        printf("Hello world\n");
+		return 1;
+}
+
---
+++ c_intro/intro.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+     printf("My name is Shrishti\n");
+	 printf("I'm from Rajasthan\n");
+	 return 0;
+}
